27de2ac18e5b7fc9e9773242ad366b8932b49cc7
Jessica  Rattani <jjrattani@gmail.com>
2020-06-26 18:25:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,47 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  public = ["chilly","cheese","bowl","lemon"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array (array, element)
+  public = ["chilly", "cheese", "bowl", "lemon"]
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  public = ["chilly", "cheese", "bowl", "lemon"]
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  public = ["chilly","cheese","bowl","lemon"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  public = ["chilly","cheese","bowl","lemon"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  public = ["chilly","cheese","bowl","lemon"]
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  public = ["chilly","cheese","bowl","lemon"]
+  array [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  public = ["chilly","cheese","bowl","lemon"]
+  array [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  public = ["chilly","cheese","bowl","lemon"]
+  array [0] = element
 end
